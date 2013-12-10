0000b5bc5273d7b4a25fb8bb660b887fc72d1583
Romain PELISSE <belaran@gmail.com>
2013-12-10 15:04:41 +0100

    add the build-all.sh script

---
+++ bin/build-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#
+
+readonly BIN_DIR=$(dirname ${0})
+
+echo 'Step 1 - Generate specfiles based on template.'
+${BIN_DIR}/generate-specs.sh
+
+echo 'Step 2 - Synchronise, if needed, source binaries with BUILDROOT.'
+${BIN_DIR}/sync-src.sh
+
+echo 'Step 3 - Build RPMS'
+${BIN_DIR}/build-rpms.sh
