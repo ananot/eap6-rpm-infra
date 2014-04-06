088ad4cea59606bb21626103c1ed8d0b3d6a0fb1
Romain PELISSE <belaran@gmail.com>
2014-04-06 14:50:40 +0200

    Remove no longer required step from build-all

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -4,11 +4,8 @@
 
 readonly BIN_DIR=$(dirname ${0})
 
-echo 'Step 1 - Generate specfiles based on template.'
-${BIN_DIR}/generate-specs.sh
-
-echo 'Step 2 - Synchronise, if needed, source binaries with BUILDROOT.'
+echo 'Step 1 - Build tarball.'
 ${BIN_DIR}/make-tarball.sh
 
-echo 'Step 3 - Build RPMS'
+echo 'Step 2 - Build RPMS'
 ${BIN_DIR}/build-rpms.sh
